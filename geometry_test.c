e7659454762a3cb42cc1111811a460a95818eab2
nmurthy9 <nischay.murthy@colorado.edu>
2017-04-26 17:14:37 -0600

    Update geometry_test.c

--- geometry_test.c
+++ geometry_test.c
@@ -54,6 +54,27 @@ START_TEST(test_2d_eq)
 }
 END_TEST
 
+/* coord_2d_area_triangle Test */
+START_TEST(test_2d_area_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+
+    a.x = 0.0;
+    a.y = 0.0;
+    b.x = 0.0;
+    b.y = 0.0;
+    c.x = 0.0;
+    c.y = 0.0;
+
+    //printf("test:%f\n", coord_2d_area_triangle(&a, &b,&c));
+    ck_assert(coord_2d_area_triangle(&a, &b,&c) == 0.0);
+
+
+}
+END_TEST
+
 /* coord_2d_dist Test */
 START_TEST(test_2d_dist)
 {
